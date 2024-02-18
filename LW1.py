7e74af8f66e696190b0ec1e3a0bebabdb7761e64
IvanMA4HEB <ifetchenko2017@gmail.com>
2024-02-18 11:44:13 +1000

    lw1fix

--- LW1.py
+++ LW1.py
@@ -1,6 +1,9 @@
 import math
-alpha = int(input("Введите значение альфа:"))
-z1 = ((1 - (2 * math.sin(alpha) * math.sin(alpha))) / (1 + math.sin(2 * alpha)))
-z2 = ((1 - math.tan(alpha)) / (1 + math.tan(alpha)))
-print("Значение z1", z1)
-print("Значение z2", z2)
+alpha = 1
+while (alpha <= 5):
+    z1 = ((1 - (2 * math.sin(alpha) * math.sin(alpha))) / (1 + math.sin(2 * alpha)))
+    z2 = ((1 - math.tan(alpha)) / (1 + math.tan(alpha)))
+    print("Значение альфа:", alpha)
+    print("Значение z1", z1)
+    print("Значение z2", z2)
+    alpha = (alpha + 1)
